81409102d4673ddccc4189901713efc621d5ee54
andythebreaker <e24074724@mail.ncku.edu.tw>
2021-04-25 03:26:40 +0800

    redo v2

---
+++ v2/routes/index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/'/*, ensureAuthenticated*/, function (req, res, next) {
+  //console.log("router.get(__/__, ensureAuthenticated, function(req, res, next) {");
+  res.render('index', { title: 'main' });
+  //res.status(200).json({"exampleKey":"exampleValue"});
+});
+
+/*function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect('/users/login');
+}*/
+
+module.exports = router;
